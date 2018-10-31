130b37547ff1d677214ec429259f0b9153439880
hardtechgroup10 <hardtech.group10@gmail.com>
2018-10-31 20:09:45 +0100

    new design

---
+++ essential.js
@@ -0,0 +1,25 @@
+function div_show() {
+    document.getElementById('pop_up_form').style.display = "block";
+    document.getElementById('overlay').style.display = "block";
+}
+
+function div_hide() {
+    document.getElementById('pop_up_form').style.display = "none";
+    document.getElementById('overlay').style.display = "none";
+}
+
+function overlay_on() {
+}
+
+function overlay_off() {
+}
+/* $(function() {
+  
+    // contact form animations
+    $('.btn-pop').click(function() {
+      $('.pop_up_form').show();
+    })
+    $('.btn-close').click(function() {
+        $('.pop_up_form').hide();
+    })    
+  }); */
