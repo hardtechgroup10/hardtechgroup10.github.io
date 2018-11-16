26272192deb8bd409dfe1e2dabf479bc4fd5e177
hardtechgroup10 <-global>
2018-11-16 16:13:24 +0100

    navbar transparent

--- essential.js
+++ essential.js
@@ -13,13 +13,3 @@ function overlay_on() {
 
 function overlay_off() {
 }
-/* $(function() {
-  
-    // contact form animations
-    $('.btn-pop').click(function() {
-      $('.pop_up_form').show();
-    })
-    $('.btn-close').click(function() {
-        $('.pop_up_form').hide();
-    })    
-  }); */
