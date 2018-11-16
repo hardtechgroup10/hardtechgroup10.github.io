5026b45435afaeb6a48fdfec161be7e363ab9c29
hardtechgroup10 <-global>
2018-11-16 20:45:58 +0100

    pic resize and responsive

--- essential.js
+++ essential.js
@@ -8,8 +8,25 @@ function div_hide() {
     document.getElementById('overlay').style.display = "none";
 }
 
-function overlay_on() {
+function myFun() {
+    if(document.getElementsByClassName("show").length) {
+        
+    }
 }
-
-function overlay_off() {
-}
+// Shortcut for $(document).ready()
+$(function() {
+    // Call on every window resize
+    $(window).resize(function(){
+        if (Modernizr.mq('(max-width: 767px)')) {
+            $('.top-fix').css({
+                'top' : '0px'
+            });
+            $('#logo').replaceWith('<img id="logo" class="img-fluid d-block" src="assets/img/logofinished-03.png">');
+        } else {
+            $('.top-fix').css({ //loop through each element with the .top-fix class
+                'top' : '-' + $('nav').outerHeight() + 'px' //adjust the css rule for margin-top to equal the element height - 10px and add the measurement unit "px" for valid CSS
+            });
+            $('#logo').replaceWith('<img id="logo" class="img-fluid d-block" src="assets/img/logofinished-04.png"> ');
+        }
+    }).resize();   // Cause an initial widow.resize to occur
+});
